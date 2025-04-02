cde2fcd618157e8b9665911252d5609dbd831487
brandonfromm <156289370+brandonfromm@users.noreply.github.com>
2025-04-02 13:36:34 -0600

    getting ready to commit

--- backend/Mission11.API/obj/Debug/net8.0/Mission11.API.AssemblyInfo.cs
+++ backend/Mission11.API/obj/Debug/net8.0/Mission11.API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Mission11.API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8ad412bafc5d4127c574b715bb502f1e962b2db2")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+538693a54ee9c0a9bc345ccb0107c76433528b0f")]
 [assembly: System.Reflection.AssemblyProductAttribute("Mission11.API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Mission11.API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
