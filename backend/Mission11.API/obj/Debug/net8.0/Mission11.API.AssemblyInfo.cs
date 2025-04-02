b51feb53fc0df748ea646556307ca16688527117
brandonfromm <156289370+brandonfromm@users.noreply.github.com>
2025-04-02 13:44:23 -0600

    Commit made from VS Code Azure Static Web Apps

--- backend/Mission11.API/obj/Debug/net8.0/Mission11.API.AssemblyInfo.cs
+++ backend/Mission11.API/obj/Debug/net8.0/Mission11.API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Mission11.API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+538693a54ee9c0a9bc345ccb0107c76433528b0f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+186d1f6a0d2b4c6000eb2c99b6e8f053c393af54")]
 [assembly: System.Reflection.AssemblyProductAttribute("Mission11.API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Mission11.API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
